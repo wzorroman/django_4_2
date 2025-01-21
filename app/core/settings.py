9c0d8fc47180bd0fa5cdc171c825d7cf48487048
wilson <wzaumarojas@gmail.com>
2025-01-20 20:31:21 -0500

    refactory app -> core (module)

--- app/core/settings.py
+++ app/core/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'app.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -77,7 +77,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
